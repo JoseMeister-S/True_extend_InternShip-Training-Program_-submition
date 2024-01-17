0bb2b7c5b64035ccab2795219d3a557db99f6289
Jose Manuel Sanchez <99229719+JoseMeister-S@users.noreply.github.com>
2024-01-17 10:25:58 -0400

    Cleaned fetch

--- src/itunes/app/scripts/route.js
+++ src/itunes/app/scripts/route.js
@@ -1,15 +1,25 @@
+import { error } from "console";
 import { NextResponse } from "next/server";
 
-async function fetchMedia(){
-    const response = await fetch('https://itunes.apple.com/search?term=jack+johnson&limit=25',
-    {"method": "GET"})
-    console.log('Response:', response);
-    
+async function fetchMedia() {
+  try {
+    const response = await fetch(
+      "https://itunes.apple.com/search?term=jack+johnson&limit=25",
+      { method: "GET" }
+    );
+    if (!response.ok) {
+      throw error(`Network response not ok: ${response.status}`);
+    }
+
     const data = await response.json();
     return data;
+  } catch (error) {
+    console.error("Error fetching media:", error);
+    throw error; // rethrow the error to indicate failure
+  }
 }
 
-export async function GET(request){
-    const media =await fetchMedia();
-    return NextResponse.json(media);
+export async function GET(request) {
+  const media = await fetchMedia();
+  return NextResponse.json(media);
 }

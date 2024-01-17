f2a65899a512d3b43e09d53b84b3928b89568da1
Jose Manuel Sanchez <99229719+JoseMeister-S@users.noreply.github.com>
2024-01-17 19:47:15 -0400

    Cleaning

--- src/itunes/app/layout.tsx
+++ src/itunes/app/layout.tsx
@@ -1,20 +1,19 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './ui/globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./ui/globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-}
+export const metadata: Metadata = {};
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
